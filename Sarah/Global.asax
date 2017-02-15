304858da78d297abaadc1cfd96ace7e918570899
Sarah Facci <sarah.e.facci@gmail.com>
2017-02-15 09:25:56 -0500

    quick prototype for responsive design. will build on this.

---
+++ Sarah/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sarah.MvcApplication" Language="C#" %>
---
+++ Sarah/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sarah.Startup))]
+namespace Sarah
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
